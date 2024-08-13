17f0ff4d6cd304a10a434ab94df5eb3dddfcaa3f
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-08-13 10:45:49 +0200

    Now is ready

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>xxxxxxxxxxx</title>
+        <style>
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 80%;
+            }
+        </style>
+    </head>
+    <body>
+        <h1><?php echo "xxxxxxxxxxxxxxxxxxx"; ?></h1>
+        <p>
+        <?php
+            echo "??????????????<br>";
+            echo "Today is" . date("d/m/Y");
+            echo "<br>.....????.....";
+        ?>
+        </p>
+        <img src="hello.gif" alt="Hola" class="center">
+    </body>
+</html>
